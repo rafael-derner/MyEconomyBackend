f65214136f5ea857abc2eef882cc530b72cff6a9
Rafael Derner <rafael-derner@live.com>
2024-06-10 10:59:10 -0300

    fix/Progresso

--- src/models/Progresso.ts
+++ src/models/Progresso.ts
@@ -9,6 +9,6 @@ export interface ProgressoMes {
 }
 
 export const getProgresso = async (usuario: number, mes: string): Promise<ProgressoMes | null> => {
-  const [rows] = await pool.query<RowDataPacket[]>('select ifnull(((sum(d.valor) / lm.valor) * 100), 0) as progresso, ifnull(sum(d.valor), 0) as gasto, lm.valor as limite, lm.mes from limiteMes lm left join despesa as d on d.limiteMesId = lm.id where lm.mes like ? and lm.usuarioId = ? group by lm.valor, lm.mes, d.id;', [mes, usuario]);
+  const [rows] = await pool.query<RowDataPacket[]>('select ifnull(((sum(d.valor) / lm.valor) * 100), 0) as progresso, ifnull(sum(d.valor), 0) as gasto, lm.valor as limite, lm.mes from limiteMes lm left join despesa as d on d.limiteMesId = lm.id where lm.mes like ? and lm.usuarioId = ? group by lm.valor, lm.mes;', [mes, usuario]);
   return rows.length ? (rows[0] as ProgressoMes) : null;
 };
